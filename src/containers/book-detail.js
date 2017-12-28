459ddd3419f5d6abef257a63d8f1bcf8e2600933
dbrudner <dbrudne1@gmail.com>
2017-12-27 22:43:23 -0500

    commit

---
+++ src/containers/book-detail.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class BookDetail extends Component {
+	render() {
+		if (!this.props.book) {
+			return <div>Select a book</div>
+		}
+
+	
+
+	
+		return (
+			<div>
+				Title: {this.props.book.title}
+				<br/>
+				Pages: {this.props.book.pages}
+			</div>
+		)
+	}
+}
+
+function mapStateToProps(state) {
+	return {
+		book: state.activeBook
+	}
+}
+
+export default connect(mapStateToProps)(BookDetail)
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+	return [
+		{title: 'title 1', pages: 105},
+		{title: 'title 2', pages: 102},
+		{title: 'title 3', pages: 107},
+		{title: 'title 4', pages: 112}
+	]
+}
